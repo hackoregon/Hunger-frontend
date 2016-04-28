683f82c3f8263a1bf92af866f8639d622a65195b
Evan Palmer <evjpalmer@gmail.com>
2016-04-28 10:14:40 -0700

    add checkboxes for other family types, rename some classes

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
         <header>
           <h1 className="main-title">Oregon Hunger Equation</h1>
         </header>
-        <section className="select-family-and-county container-fluid">
+        <section className="family-and-county-section container-fluid">
           <div className="row">
             <div className="col-xs-12">
                 <p className="text-center">Project Mission Statement:</p>
@@ -19,7 +19,7 @@ export default class App extends React.Component {
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Blah.
                 </p>
                 <div className="dropdown county-dropdown">
-                <p className="text-center">Select a County:</p>
+                <p className="select-county-p text-center">Select a County:</p>
                   <button className="btn btn-default dropdown-toggle county-dropdown-toggle" type="button" id="county-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                     Dropdown
                     <span className="caret"></span>
@@ -30,8 +30,8 @@ export default class App extends React.Component {
                     <li><a href="#">Something else here</a></li>
                   </ul>
                 </div>
-                <p className="text-center">Select a Family Type:</p>
                 <div className="row family-types-wrapper">
+                  <p className="select-family-type-p text-center">Select a Family Type:</p>
                   <div className="col-xs-12 col-sm-4 family-type-div">
                     <div className="text-center">
                       <label id="label-single-adult" className="family-type-label">
@@ -43,12 +43,24 @@ export default class App extends React.Component {
                       </div>
                   </div>
                   <div className="col-xs-12 col-sm-4 family-type-div">
-                    <img className="img-responsive center-block"
-                      src="dist/images/HO_familyType_parent_twoKids_color.svg" alt="a single parent with two children" />
+                    <div className="text-center">
+                      <label id="label-single-parent" className="family-type-label">
+                        <img className="img-responsive center-block"
+                          src="dist/images/HO_familyType_parent_twoKids_color.svg" alt="a single parent with two children" />
+                        <input type="checkbox" className="family-type-checkbox" />
+                        Single Parent, Two Children
+                      </label>
+                    </div>
                   </div>
                   <div className="col-xs-12 col-sm-4 family-type-div">
-                    <img className="img-responsive center-block"
-                      src="dist/images/HO_familyType_twoParent_twoKids_color.svg" alt="two parents with two children" />
+                    <div className="text-center">
+                      <label id="label-two-parents" className="family-type-label">
+                        <img className="img-responsive center-block"
+                        src="dist/images/HO_familyType_twoParent_twoKids_color.svg" alt="two parents with two children" />
+                        <input type="checkbox" className="family-type-checkbox" />
+                        Two Parents, Two Children
+                      </label>
+                    </div>
                   </div>
                 </div>
             </div>
