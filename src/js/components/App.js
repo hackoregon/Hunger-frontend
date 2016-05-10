91f42bae5d5ace05b6fa352be62f5faadfe70e66
Evan Palmer <evjpalmer@gmail.com>
2016-05-10 11:49:20 -0700

    move mission statement to its own section

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -25,6 +25,12 @@ export default class App extends React.Component {
         <header>
           <h1 className="main-title">Oregon Hunger Equation</h1>
         </header>
+        <section className="mission-statement container-fluid">
+          <p>Project Mission Statement:</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Blah.
+          </p>
+        </section>
         <HorizontalRule>
           <h2 className="hr-content">Select a County</h2>
         </HorizontalRule>
@@ -34,10 +40,6 @@ export default class App extends React.Component {
         <section className="family-and-county-section container-fluid">
           <div className="row">
             <div className="col-xs-12">
-                <p className="text-center">Project Mission Statement:</p>
-                <p className="text-center">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Blah.
-                </p>
                 <div className="dropdown county-dropdown">
                 <p className="select-county-p text-center">Select a County:</p>
                   <button className="btn btn-default dropdown-toggle county-dropdown-toggle" type="button" id="county-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
