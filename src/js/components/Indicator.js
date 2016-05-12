eee6168d0e3929533407c1086bb423aee9d7a836
Evan Palmer <evjpalmer@gmail.com>
2016-05-11 19:54:07 -0700

    add markup and styles for indicator line
    remove Bootstrap markup from the component
    styles are kind of brittle at this point

--- src/js/components/Indicator.js
+++ src/js/components/Indicator.js
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { PropTypes } from 'react'
 require('../../styles/main.scss')
 
 export default class Indicator extends React.Component {
@@ -8,13 +7,18 @@ export default class Indicator extends React.Component {
   }
   render() {
     return (
-      <div className="row">
-        <div className="col-xs-12 col-sm-4">
-          <div className="indicator-blocks">
+      <div className="indicator-root">
+        <div className="indicator-outer-wrapper">
+          <div className="indicator-blocks-wrapper">
             <span className="ind-block block-1"></span>
             <span className="ind-block block-2"></span>
             <span className="ind-block block-3"></span>
             <span className="ind-block block-4"></span>
+          </div>
+          <div className="indicator-line-wrapper">
+            <div className="indicator-arrow top-arrow"></div>
+            <div className="indicator-line"></div>
+            <div className="indicator-arrow bottom-arrow"></div>
           </div>
         </div>
       </div>
