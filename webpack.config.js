4528bc09e63b9283033e6eadff77526d3b657926
Dan Schuman <danschuman@gmail.com>
2016-05-22 11:58:36 -0700

    Add eslint; minor spacing tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,36 @@
-var path = require('path')
 module.exports = {
-    entry: './main.js',
-    output: {
-        path: __dirname,
-        filename: 'index.js'
-    },
-    devServer: {
-        inline: true,
-        port: 3333
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {
-              test: /\.css$/,
-              loaders: ["style", "css"]
-            },
-            {
-              test: /\.(eot|ttf|woff|woff2)$/,
-              loader: 'file?name=src/styles/fonts/[name].[ext]'
-            },
-            {
-              test: /(layers.*|marker-.*)\.png$/,
-              loader: 'file?name=node_modules/leaflet/dist/images/[name].[ext]'
-            }
-        ]
-    }
+  entry: './main.js',
+  output: {
+    path: __dirname,
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file?name=src/styles/fonts/[name].[ext]'
+      },
+      {
+        test: /(layers.*|marker-.*)\.png$/,
+        loader: 'file?name=node_modules/leaflet/dist/images/[name].[ext]'
+      }
+    ]
+  }
 }
