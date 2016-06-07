f6f725933fd6f64663d5c4712c6e868a740603f5
Evan Palmer <evjpalmer@gmail.com>
2016-06-07 16:26:07 -0700

    add className navbar-static-top to nav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -166,8 +166,7 @@ export default class App extends React.Component {
     return (
       <div>
         <header>
-
-          <nav className="navbar navbar-default">
+          <nav className="navbar navbar-default navbar-static-top">
             <div className="container-fluid">
               <div className="navbar-header">
                 <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
