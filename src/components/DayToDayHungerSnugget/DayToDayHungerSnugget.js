a4f603f535d6ddd8df5d0d713e83d530a03ccb7e
Evan Palmer <evjpalmer@gmail.com>
2016-06-15 13:08:05 -0700

    use RATINGS for snugget switch cases, match dummy text

--- src/components/DayToDayHungerSnugget/DayToDayHungerSnugget.js
+++ src/components/DayToDayHungerSnugget/DayToDayHungerSnugget.js
@@ -6,23 +6,24 @@ require('./DayToDayHungerSnugget.css')
 const DayToDayHungerSnugget = (props) => {
   const { RATINGS } = constants
   let content
+  console.log("RATINGS:", RATINGS)
   console.log("snugget securityStatus:", props.securityStatus)
   switch (props.securityStatus) {
-    case 1:
+    case RATINGS['extremely insecure']:
       content = (<p className="day-to-day-hunger-snugget">
-          You are very insecure. You're missing <span className="meals-missed">{props.mealsMissed}</span> meals.</p>)
+          You are extremely insecure. You're missing <span className="meals-missed">{props.mealsMissed}</span> meals.</p>)
       break
-    case 2:
+    case RATINGS['highly insecure']:
       content = (<p className="day-to-day-hunger-snugget">
-          You are moderately insecure. You're missing <span className="meals-missed">{props.mealsMissed}</span> meals.</p>)
+          You are highly insecure. You're missing <span className="meals-missed">{props.mealsMissed}</span> meals.</p>)
       break
-    case 3:
+    case RATINGS['moderately insecure']:
       content = (<p className="day-to-day-hunger-snugget">
         You are moderately insecure. You're not missing meals, but you might struggle if you have unexpected expenses.</p>)
       break
-    case 4:
+    case RATINGS['secure']:
       content = (<p className="day-to-day-hunger-snugget">
-        You are very secure. You're not missing meals, and can consistently afford to buy the food you need.</p>)
+        You are secure. You're not missing meals, and can consistently afford to buy the food you need.</p>)
       break
     default:
       content = (<p>Unknown security rating.</p>)
