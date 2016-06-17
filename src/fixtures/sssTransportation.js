85538d97aa2cabe4aa65445dcdec93f8714446b5
Evan Palmer <evjpalmer@gmail.com>
2016-06-16 21:25:26 -0700

    add fixture data for sssMiscellaneous and sssTransportation

---
+++ src/fixtures/sssTransportation.js
@@ -0,0 +1,184 @@
+export const sssTransportation = {
+     "41071":  {
+    "transportation_1":270.1328796
+    ,"transportation_3":278.4928796
+    ,"transportation_4":529.5710565
+  },
+     "41069":  {
+    "transportation_1":240.9042698
+    ,"transportation_3":249.2642698
+    ,"transportation_4":475.2418056
+  },
+     "41067":  {
+    "transportation_1":279.7548182
+    ,"transportation_3":288.1148182
+    ,"transportation_4":548.8149338
+  },
+     "41065":  {
+    "transportation_1":245.0328895
+    ,"transportation_3":253.3928895
+    ,"transportation_4":483.499045
+  },
+     "41063":  {
+    "transportation_1":240.3030286
+    ,"transportation_3":248.6630286
+    ,"transportation_4":474.039323
+  },
+     "41061":  {
+    "transportation_1":238.6599959
+    ,"transportation_3":247.0199959
+    ,"transportation_4":470.7532577
+  },
+     "41059":  {
+    "transportation_1":239.5720439
+    ,"transportation_3":247.9320439
+    ,"transportation_4":472.5773537
+  },
+     "41057":  {
+    "transportation_1":242.1096241
+    ,"transportation_3":250.4696241
+    ,"transportation_4":477.6525141
+  },
+     "41055":  {
+    "transportation_1":247.5893165
+    ,"transportation_3":255.9493165
+    ,"transportation_4":488.6118989
+  },
+     "41053":  {
+    "transportation_1":272.8629658
+    ,"transportation_3":281.2229658
+    ,"transportation_4":535.031229
+  },
+     "41051":  {
+    "transportation_1":100
+    ,"transportation_3":100
+    ,"transportation_4":200
+  },
+     "41049":  {
+    "transportation_1":241.8558661
+    ,"transportation_3":250.2158661
+    ,"transportation_4":477.1449981
+  },
+     "41047":  {
+    "transportation_1":279.1396064
+    ,"transportation_3":287.4996064
+    ,"transportation_4":547.5845103
+  },
+     "41045":  {
+    "transportation_1":238.7818266
+    ,"transportation_3":247.1418266
+    ,"transportation_4":470.9969192
+  },
+     "41043":  {
+    "transportation_1":236.9994626
+    ,"transportation_3":245.3594626
+    ,"transportation_4":467.4321912
+  },
+     "41041":  {
+    "transportation_1":241.9548115
+    ,"transportation_3":250.3148115
+    ,"transportation_4":477.3428889
+  },
+     "41039":  {
+    "transportation_1":237.7634291
+    ,"transportation_3":246.1234291
+    ,"transportation_4":468.9601241
+  },
+     "41037":  {
+    "transportation_1":238.1625763
+    ,"transportation_3":246.5225763
+    ,"transportation_4":469.7584185
+  },
+     "41035":  {
+    "transportation_1":240.4403406
+    ,"transportation_3":248.8003406
+    ,"transportation_4":474.3139471
+  },
+     "41033":  {
+    "transportation_1":238.4116226
+    ,"transportation_3":246.7716226
+    ,"transportation_4":470.2565112
+  },
+     "41031":  {
+    "transportation_1":242.2038578
+    ,"transportation_3":250.5638578
+    ,"transportation_4":477.8409816
+  },
+     "41029":  {
+    "transportation_1":234.2747612
+    ,"transportation_3":242.6347612
+    ,"transportation_4":461.9827882
+  },
+     "41027":  {
+    "transportation_1":244.7017252
+    ,"transportation_3":253.0617252
+    ,"transportation_4":482.8367164
+  },
+     "41025":  {
+    "transportation_1":236.5363711
+    ,"transportation_3":244.8963711
+    ,"transportation_4":466.5060081
+  },
+     "41023":  {
+    "transportation_1":238.2870995
+    ,"transportation_3":246.6470995
+    ,"transportation_4":470.0074649
+  },
+     "41021":  {
+    "transportation_1":242.6083899
+    ,"transportation_3":250.9683899
+    ,"transportation_4":478.6500456
+  },
+     "41019":  {
+    "transportation_1":237.1697565
+    ,"transportation_3":245.5297565
+    ,"transportation_4":467.7727789
+  },
+     "41017":  {
+    "transportation_1":240.5298626
+    ,"transportation_3":248.8898626
+    ,"transportation_4":474.4929912
+  },
+     "41015":  {
+    "transportation_1":239.1298184
+    ,"transportation_3":247.4898184
+    ,"transportation_4":471.6929027
+  },
+     "41013":  {
+    "transportation_1":236.7524356
+    ,"transportation_3":245.1124356
+    ,"transportation_4":466.9381371
+  },
+     "41011":  {
+    "transportation_1":236.8123413
+    ,"transportation_3":245.1723413
+    ,"transportation_4":467.0579486
+  },
+     "41009":  {
+    "transportation_1":277.7664861
+    ,"transportation_3":286.1264861
+    ,"transportation_4":544.8382697
+  },
+     "41007":  {
+    "transportation_1":242.5976203
+    ,"transportation_3":250.9576203
+    ,"transportation_4":478.6285065
+  },
+     "41005":  {
+    "transportation_1":278.9780629
+    ,"transportation_3":287.3380629
+    ,"transportation_4":547.2614232
+  },
+     "41003":  {
+    "transportation_1":234.5211151
+    ,"transportation_3":242.8811151
+    ,"transportation_4":462.4754961
+  },
+     "41001":  {
+    "transportation_1":238.4991254
+    ,"transportation_3":246.8591254
+    ,"transportation_4":470.4315167
+  }
+}
+
+export default sssTransportation
