a5e446776f41bb0536bd926f138305ef5651abd0
Evan Palmer <evjpalmer@gmail.com>
2016-06-20 15:08:49 -0700

    add DataDeepDive placeholder content

--- src/components/DataDeepDive/DataDeepDive.js
+++ src/components/DataDeepDive/DataDeepDive.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const DataDeepDive = () => (<div className="hunger-page-root">This is the Data Deep Dive page.</div>)
+const DataDeepDive = () => (
+  <div className="hunger-page-root">
+    <h1>Data Deep Dive</h1>
+    <p>More content coming soon...</p>
+    <p>In the meantime, here is a partial list of data sources used in the project:</p>
+    <ul className="deep-dive-source-list">
+      <li></li>
+    </ul>
+
+
+  </div>
+)
 
 export default DataDeepDive
