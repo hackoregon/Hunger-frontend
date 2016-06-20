143c0198c0605d2f360ee8513caf08010c95e8cb
Evan Palmer <evjpalmer@gmail.com>
2016-06-20 11:38:11 -0700

    add About and DataDeepDive dummy pages, add browserHistory

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './src/components/App/App.js'
-import { Router, Route } from 'react-router'
-import Team from './src/components/Team/Team.js'
+import { Router, Route, browserHistory } from 'react-router'
+import Team from './src/components/Team/Team'
+import About from './src/components/About/About'
+import DataDeepDive from './src/components/DataDeepDive/DataDeepDive'
 
 ReactDOM.render((
-  <Router>
+  <Router history={browserHistory}>
     <Route path="/" component={App} />
-    <Route path="/team" component={Team} />
+    <Route path="about" component={About} />
+    <Route path="team" component={Team} />
+    <Route path="data-deep-dive" component={DataDeepDive} />
   </Router>
 ),
   document.getElementById('app-container'))
---
+++ src/components/About/About.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const About = () => (<div>This is the About page.</div>)
+
+export default About
