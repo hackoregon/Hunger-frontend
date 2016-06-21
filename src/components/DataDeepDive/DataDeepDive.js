bed6b296cd332c684ed58f4569cc04b2f8a39c99
Evan Palmer <evjpalmer@gmail.com>
2016-06-20 17:33:55 -0700

    add links to data deep dive page

--- src/components/DataDeepDive/DataDeepDive.js
+++ src/components/DataDeepDive/DataDeepDive.js
@@ -6,7 +6,10 @@ const DataDeepDive = () => (
     <p>More content coming soon...</p>
     <p>In the meantime, here is a partial list of data sources used in the project:</p>
     <ul className="deep-dive-source-list">
-      <li></li>
+      <li><a href="http://raiseeffect.org/#/about?_k=33mbvy">http://raiseeffect.org/#/about?_k=33mbvy</a></li>
+      <li><a href="http://agsci.oregonstate.edu/sites/agscid7/files/oregonhungerreportjune2016.pdf">http://agsci.oregonstate.edu/sites/agscid7/files/oregonhungerreportjune2016.pdf</a></li>
+      <li><a href="https://oregonhunger.org/hunger-in-oregon">https://oregonhunger.org/hunger-in-oregon</a></li>
+      <li><a href="http://map.feedingamerica.org/county/2014/overall/oregon">http://map.feedingamerica.org/county/2014/overall/oregon</a></li>
     </ul>
 
 
