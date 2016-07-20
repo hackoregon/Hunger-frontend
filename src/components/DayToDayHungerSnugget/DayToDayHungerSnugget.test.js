e1695a44671e51acfd3118721921f82a79a1489c
Evan Palmer <evjpalmer@gmail.com>
2016-07-19 21:41:23 -0700

    uncomment hasHeading test, add hasText test

--- src/components/DayToDayHungerSnugget/DayToDayHungerSnugget.test.js
+++ src/components/DayToDayHungerSnugget/DayToDayHungerSnugget.test.js
@@ -10,10 +10,13 @@ const minProps = {
 
 describe('DayToDayHungerSnugget', function() {
   it('has a heading', function hasHeading() {
-    // expect(shallow(<Snugget {...minProps} />).find('.snugget-heading').length)
-    //   .toEqual(1)
+    expect(shallow(<Snugget {...minProps} />).find('.snugget-heading').length)
+      .toEqual(1)
   })
-  it('has some text content')
+  it('has some text content', function hasText() {
+    expect(shallow(<Snugget {...minProps} />).find('.snugget-text').text().length)
+      .toBeGreaterThan(0)
+  })
   it('has the correct heading color for sufficient families')
   it('has the correct heading color for vulnerable families')
   it('has the correct text for a sufficient single individual')
