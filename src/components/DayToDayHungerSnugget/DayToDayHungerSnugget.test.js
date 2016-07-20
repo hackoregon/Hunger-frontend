9356f941b6517e1282e0cbbccfab3032623d85b4
Evan Palmer <evjpalmer@gmail.com>
2016-07-20 11:11:57 -0700

    add correctTextSufficientSingle test

--- src/components/DayToDayHungerSnugget/DayToDayHungerSnugget.test.js
+++ src/components/DayToDayHungerSnugget/DayToDayHungerSnugget.test.js
@@ -81,7 +81,12 @@ describe('DayToDayHungerSnugget', function() {
     .prop('style').color)
     .toEqual('#b5441d')
   })
-  it('has the correct text for a sufficient single individual')
+  it('has the correct text for a sufficient single individual', function correctTextSufficientSingle() {
+    const snuggetText = "You do not have trouble putting food on the table. Your meals are complete and you generally do not skip meals. You are either not eligible for benefits or your benefits cover your meals sufficiently."
+
+    expect(shallow(<Snugget individuals={1} securityStatus={3} />).find('.snugget-text').text())
+      .toEqual(snuggetText)
+  })
   it('has the correct text for a sufficient family of three')
   it('has the correct text for a sufficient family of four')
 })
